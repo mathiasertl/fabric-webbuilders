c7a9ab5fe6afa16802fdc078069588d4311b413e
Mathias Ertl <mati@er.tl>
2014-12-06 15:13:43 +0100

    add jsmin task

--- fabric_webbuilders/__init__.py
+++ fabric_webbuilders/__init__.py
@@ -2,3 +2,4 @@
 from fabric_webbuilders.jquery import BuildJqueryTask
 from fabric_webbuilders.bootstrap import BuildBootstrapTask
 from fabric_webbuilders.cssmin import MinifyCSSTask
+from fabric_webbuilders.jsmin import MinifyJSTask
